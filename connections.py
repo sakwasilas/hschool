059fd4c2e99212b3379321d3c789d771312d7f16
sakwasilas <sakwahsilas@gmail.com>
2025-10-16 09:52:22 +0300

    updated

--- connections.py
+++ connections.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
-import os
 
-DATABASE_URL = "postgresql://wifi_0aqg_user:UJbhFEzLazjs0usFRgxx5q0C8H4O6pwf@dpg-d3nno8idbo4c73d57oo0-a.oregon-postgres.render.com:5432/wifi_0aqg?sslmode=require"
+# =========================
+# New Database URL for hschool
+# =========================
+DATABASE_URL = "postgresql://hschool_user:MxBBT8XJjKoM6ZZPwR4HvWbjWRfHvgUc@dpg-d3o99njipnbc73fq0am0-a.oregon-postgres.render.com:5432/hschool?sslmode=require"
 
+# Create engine
 engine = create_engine(DATABASE_URL, echo=True)
+
+# Create session
 Session = scoped_session(sessionmaker(bind=engine))
 SessionLocal = Session
+
+# Base declarative class
 Base = declarative_base()
